d08386fc14fc2f2886edebdb7affb92e88304650
kissmett <kissmett@163.com>
2023-06-15 10:49:01 +0800

    在点击位置显示随机大小花色的牌

--- PlayCards.py
+++ PlayCards.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import os
 
 RED = (255,0,0)
 GREEN = (0,255,0)
@@ -11,22 +12,28 @@ screen = pygame.display.set_mode([800,600])
 pygame.display.set_caption("UE")
 timer = pygame.time.Clock()
 
-class Card(pygame.sprite.Sprite):    
-    def __init__(self,imagefile,pos):
+class Card(pygame.sprite.Sprite): 
+    # 属性image/rect不可少
+    def __init__(self,imagefile,pos,scale):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(imagefile)
         self.pos = pos
+        self.scale = scale
+        self.image = pygame.transform.scale(self.image,
+                                            (self.image.get_rect().width*self.scale,
+                                             self.image.get_rect().height*self.scale)
+                                             )
         self.rect = self.image.get_rect()
-        self.rect.x = self.pos[0]
-        self.rect.y = self.pos[1]
+        self.rect.x = self.pos[0] - self.rect.width/2
+        self.rect.y = self.pos[1] - self.rect.height/2
         pass
     def update(self):
-        self.rect.x = self.pos[0]
-        self.rect.y = self.pos[1]
+        # self.rect.x = self.pos[0] - self.rect.width/2
+        # self.rect.y = self.pos[1] - self.rect.height/2
         pass
 
 sprite_list = pygame.sprite.Group()
-
+imagefile_list = [ f for f in os.listdir('./assets_en') if f.rfind('.jpg')>0 ]
 keep_going=True        
 while keep_going:
     for event in pygame.event.get():
@@ -34,7 +41,7 @@ while keep_going:
             keep_going = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = event.pos
-            sprite = Card('assets_en/Clubs2.jpg',pos)  
+            sprite = Card('assets_en/'+random.choice(imagefile_list),pos,random.random())  
             sprite_list.add(sprite)
     screen.fill(BLACK)        
     sprite_list.update()
